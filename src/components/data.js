b7fea2e0fc95df2ecb97e27249d2d81079b9dc2d
ayush-raj8 <ayushraj763@gmail.com>
2023-07-28 13:45:32 +0530

    commit

--- src/components/data.js
+++ src/components/data.js
@@ -19,7 +19,7 @@ const data1 = {
             "time":"Aug 2021 - Dec 2021"
         },
         {
-            "title": "BITS Pilani Goa",
+            "title": "BITS Pilani,Goa",
             "subtitle": "Undergraduate Teaching Assistant",
             "summary": "Guiding the freshers towards computer programming in C language (Course CS F111).",
             "time":"Mar 2021 - Jun 2021"
@@ -60,12 +60,17 @@ const data1 = {
             "title": "Iris Classifier Android App",
             "subtitle": "Android App, Machine Learning,Java,Python",
             "summary": "Built an Android app classifies Iris flowers based on inputs containing shape and sizes of the flower. It also maintains a history of all sata enters using sqllite locally."
+        },
+        {
+            "title": "Multi App Search Enginge",
+            "subtitle": "Flask,React Js,Python",
+            "summary": "Built a ReactJs Flask App to search across multiple apps like jira,confluence,slack and agrregate the results."
         }
     ],
     "Published Research Papers": [
         {
             "title":"Towards Efficient Emotion Self-report Collection Using Human-AI Collaboration: A Case Study on Smartphone Keyboard Interaction",
-            "subtitle":"AI,Active Learning,Python, ACM",
+            "subtitle":"Active Learning,Association for Computing Machinery",
             "summary":"Using active learning we were able  fewer self-reports by 46% than the baselines to train the emotion self-report detection model and yet outperforms the baselines with an average self-report detection F-score of 85%",
             "url":"https://dl.acm.org/doi/10.1145/3596269"
         }
@@ -95,12 +100,6 @@ const data1 = {
             "subtitle": "Operating Systems, C, Processes",
             "summary": "This article introduces interprocess communication in OS and discusses the need for the same. This article also covers different ways to implement ipc in OS using C language.",
             "url": "https://www.scaler.com/topics/operating-system/inter-process-communication-in-os/"
-        },
-        {
-            "title": "log() in Java",
-            "subtitle": "Java, maths",
-            "summary": "This article explore the usecases of log() function of java.",
-            "url": "https://www.scaler.com/topics/log-in-java/"
         }
     ]
 };
