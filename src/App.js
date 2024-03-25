26b3e013a51a9a8b72c62ece47d191698f042cb7
ayush-raj8 <ayushraj763@gmail.com>
2024-03-25 10:10:01 +0530

    HashRouter added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header  from './components/Header'
 import Footer  from './components/Footer'
 import Starter from './components/Starter';
 import DataAccordion from './components/DataAccordion';
-import {BrowserRouter as Router } from  'react-router-dom';
+import {HashRouter as Router } from  'react-router-dom';
 import GetInTouch from './components/GetInTouch';
 
 function App() {
