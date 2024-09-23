f9d529fb6025be2baf8517f2f1e709ffe2e610ba
ayush-raj8 <ayushraj763@gmail.com>
2024-09-23 12:08:12 +0530

    !important Added

--- src/components/DataAccordion.js
+++ src/components/DataAccordion.js
@@ -63,7 +63,7 @@ function DataAccordion() {
                                     </StepLabel>
                                     <StepContent sx={{
                                         mt: 2,
-                                        backgroundColor: 'rgba(255, 255, 255, 0.1)',
+                                        backgroundColor: 'rgba(255, 255, 255, 0.1)!important',
                                         backdropFilter: 'blur(10px)',
                                         borderRadius: '10px',
                                         padding: '10px',
